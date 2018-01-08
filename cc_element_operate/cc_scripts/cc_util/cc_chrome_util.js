a857214caf6ade25e2ce51539acbaadeb83b914b
hello_chenchen <welcomechenchen@gmail.com>
2018-01-09 03:15:21 +0800

    添加后端日志功能

--- cc_element_operate/cc_scripts/cc_util/cc_chrome_util.js
+++ cc_element_operate/cc_scripts/cc_util/cc_chrome_util.js
@@ -1,33 +1,43 @@
-/**
+var cc_chrome_util = {/**
  * @description 导入js脚本
  * @argments 可变参数, 需要导入的js脚本文件path
  * @returns
  * @author hello_chenchen
  */
-function importJsFiles() {
-    for (param in arguments) {
-        chrome.tabs.executeScript(null, { file: param }, null);
-        sendMessage2Content(null, {'consoleLog': param}, null);
+    importJsFiles: function () {
+        for (var i = 0; i > arguments.length; i++) {
+            alert("arguments:" + arguments[i]);
+            chrome.tabs.executeScript(null, { file: arguments[i] }, null);
+            sendMessage2Content({ 'consoleLog': param });
+        }
+    },
+
+    /**
+     * 
+     */
+    getCurrentTabUrl: function () {
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function (tabs) {
+            var tab = tabs[0];
+            var url = tab.url;
+            return url;
+        });
+    },
+
+    /**
+     * 
+     * @param {*} tabId 
+     * @param {*} message 
+     * @param {*} responseCallBack 
+     */
+    sendMessage2Content: function(message) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id,
+                { methond: "cc_util.consoleLog", params: [message] },
+                null);
+        });
     }
-}
 
-function getCurrentTabUrl () {
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function (tabs) {
-        var tab = tabs[0];
-        var url = tab.url;
-        return url;
-    });
-}
-
-/**
- * 
- * @param {*} tabId 
- * @param {*} message 
- * @param {*} responseCallBack 
- */
-function sendMessage2Content(tabId, message, responseCallBack) {
-    chrome.tabs.sendMessage(tabId, message, responseCallBack);
 }
