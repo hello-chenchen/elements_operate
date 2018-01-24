26677006ba1da3d3664a6cb87e34ceebe9c34fb1
hello_chenchen <welcomechenchen@gmail.com>
2018-01-25 01:16:31 +0800

    添加鼠标悬停后离开的动作

--- cc_element_operate/cc_scripts/cc_util/cc_elements.js
+++ cc_element_operate/cc_scripts/cc_util/cc_elements.js
@@ -1,7 +1,14 @@
 var cc_elements = {
     targetElement: null,
+    originalOutLineStyle: "",
 
-    highLightElements: function() {
+    highLightElement: function() {
+
+        cc_elements.originalOutLineStyle = cc_elements.targetElement.style.outline;
         cc_elements.targetElement.style.outline = "solid 6px rgba(255,0,0,0.5)";
+    },
+
+    removeHighLightElement: function() {
+        cc_elements.targetElement.style.outline = cc_elements.originalOutLineStyle;
     }
 }
