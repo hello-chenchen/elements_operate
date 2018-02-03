7808d3f07d896ea75b807f86676880d29f68c0d9
hello_chenchen <welcomechenchen@gmail.com>
2018-02-04 03:26:05 +0800

    修改类结构和方法可见性，未完成

--- cc_element_operate/cc_scripts/cc_util/cc_elements.js
+++ cc_element_operate/cc_scripts/cc_util/cc_elements.js
@@ -12,3 +12,13 @@ var cc_elements = {
         cc_elements.targetElement.style.outline = cc_elements.originalOutLineStyle;
     }
 }
+
+function hc_elements(){
+    var targetElement = null;
+    var originalOutLineStyle = "";
+
+    this.highLightElement = function() {
+        originalOutLineStyle = targetElement.style.outline;
+        targetElement.style.outline = "solid 6px rgba(255,0,0,0.5)";
+    }
+}
--- cc_element_operate/cc_scripts/cc_util/cc_mouse.js
+++ cc_element_operate/cc_scripts/cc_util/cc_mouse.js
@@ -15,3 +15,24 @@ var cc_mouse = {
         cc_elements.highLightElement();
     }
 }
+
+function hc_mouse () {
+    this.mouseOverAction = function (windowsEvent) {
+        if(cc_elements.targetElement != null)
+        {
+            cc_elements.removeHighLightElement();
+        }
+
+        cc_elements.targetElement = windowsEvent.target;
+
+        cc_elements.highLightElement();
+    }
+
+    var privateMouse = 18;
+
+    this.publicMouse = 22;
+
+    this.getPrivateMouse = function() {
+        return privateMouse;
+    };
+}
