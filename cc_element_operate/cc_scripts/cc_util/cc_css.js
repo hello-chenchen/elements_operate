db19dc5916832fb01ff6543dea6b27b6f2bdea91
hello_chenchen <welcomechenchen@gmail.com>
2018-02-08 02:16:01 +0800

    添加bootscript控件

--- cc_element_operate/cc_scripts/cc_util/cc_css.js
+++ cc_element_operate/cc_scripts/cc_util/cc_css.js
@@ -2,10 +2,24 @@
  * css样式类
  */
 var cc_css = {
+    cc_button_test : "",
+
     //添加元素边的高亮
     cc_sideHighLight : "solid 6px rgba(255,0,0,0.5)",
     //
     cc_sigleSelectTable : "<div class='btn-group'></div>",
-    
-    cc_botton_test : "" //<button></button>
+
+    cc_popmenu : '\
+    <div class="btn-group">\
+        <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
+            Action\
+        </button>\
+        <div class="dropdown-menu">\
+            <a class="dropdown-item" href="#">Action</a>\
+            <a class="dropdown-item" href="#">Another action</a>\
+            <a class="dropdown-item" href="#">Something else here</a>\
+            <div class="dropdown-divider"></div>\
+            <a class="dropdown-item" href="#">Separated link</a>\
+        </div>\
+    </div>'
 }
