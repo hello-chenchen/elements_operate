6a7f5e9621ca77aa1d0624da42e93d0b75893236
hello_chenchen <welcomechenchen@gmail.com>
2018-02-12 20:22:33 +0800

    添加"mouseenter","mouseleave"和"mouseout"事件

--- cc_element_operate/content.js
+++ cc_element_operate/content.js
@@ -11,15 +11,27 @@ var cc_content = {
             cc_util.executeFunctionByName(message.methond, window, message.params);
         });
 
+        // document.addEventListener("mouseenter", function() {
+        //     cc_content.m_mouse.mouseEnterAction(window.event);
+        // }, true);
+
+        // document.addEventListener("mouseleave", function () {
+        //     cc_content.m_mouse.mouseLeaveAction(window.event);
+        // }, true);
+
         //注册mouseover事件
         document.addEventListener("mouseover", function() {
             cc_content.m_mouse.mouseOverAction(window.event);
         }, true);
 
-        //注册mousedown事件
-        document.addEventListener("mousedown", function(){
-            cc_content.m_mouse.mouseDownAction(window.event);
+        document.addEventListener("mouseout", function() {
+            cc_content.m_mouse.mouseOutAction(window.event);
         }, true);
+
+        //注册mousedown事件
+        // document.addEventListener("mousedown", function(){
+        //     cc_content.m_mouse.mouseDownAction(window.event);
+        // }, true);
 
         //获取当前网页主机名
         var currentDomain = window.location.host;
