0c5b9ed51aeac38aeb758c9662416169026b2f50
hello_chenchen <welcomechenchen@gmail.com>
2018-09-11 01:30:07 +0800

    添加hot key

--- cc_element_operate/background.js
+++ cc_element_operate/background.js
@@ -1,14 +1,18 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     //只有页面完成才加载
     if ("complete" == changeInfo.status) {
-        cc_chrome_util.sendMessage2Content("cc_util.consoleLog", "hello world");
+        cc_chrome_util.sendMessage2Content("cc_util.consoleLog", "hello world!");
         //判断url是否是localStorage中存储，是则执行下面的操作
         // isExistUrl();
     }
 });
 
 chrome.runtime.onMessage.addListener(
-    function (message, sender, sendResponse){
+    function (message, sender, sendResponse) {
         cc_util.executeFunctionByName(message.methond, window, message.params);
         // alert("cc_methond:" + message.methond + message.params);
+    });
+
+chrome.commands.onCommand.addListener(function(command) {
+    cc_chrome_util.sendMessage2Content("cc_util.consoleLog", command);
     });
